ddfea40c75a55700bc2541b67c494ec04fefea8e
Estrada <willeduard@gmail.com>
2021-03-30 21:45:50 -0500

    Creación de la Aplicación web

---
+++ CNTI365.FACTUR.WEB/CNTI365.FACTUR/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CNTI365.FACTUR
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
