4077318693270c5da6450fda9f101e3d7b0d6300
Estrada <willeduard@gmail.com>
2021-03-31 23:41:49 -0500

    Diseño de la vista empresa

---
+++ CNTI365.FACTUR.WEB/CNTI365.FACTUR/Controllers/RegistroEmpresaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CNTI365.FACTUR.Controllers
+{
+    public class RegistroEmpresaController : Controller
+    {
+        // GET: RegistroEmpresa
+        public ActionResult RegistroEmpresa()
+        {
+            return View();
+        }
+
+    }
+}
