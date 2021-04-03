7de14029ee6c09ab02c6ba37e6ff13724098c8cd
Estrada <willeduard@gmail.com>
2021-04-02 21:15:41 -0500

    Crear archivo js y previsualizar la imagen con javascrip

--- CNTI365.FACTUR.WEB/CNTI365.FACTUR/Scripts/app/registroempresa.js
+++ CNTI365.FACTUR.WEB/CNTI365.FACTUR/Scripts/app/registroempresa.js
@@ -1,31 +1,6 @@
 ﻿
 /*Visualizar imagen*/
-/*
-$("#imagen").change(function () {
 
-    let imagen = this.files[0];
-
-    if (imagen["type"] != "image/jpeg" && imagen["type"] != "image/png") {
-        $("#imagen").val("");
-        $(".previsualizar").attr("src", "../Content/img/img_logo/TuLogo.png");
-        alert("Debe subir una imagen en formato jpeg o png");
-    } else if (imagen["size"] > 2000000) {
-        $("#imagen").val("");
-        $(".previsualizar").attr("src", "../Content/img/img_logo/TuLogo.png");
-        alert("La imagen debe tener como maximo 2MB");
-    } else {
-        var datosImagen = new FileReader;
-        datosImagen.readAsDataURL(imagen);
-
-        $(datosImagen).on("load", function (event) {
-            var rutaImagen = event.target.result;
-            $(".previsualizar").attr("src", rutaImagen);
-        })
-    }
-
-})
-
-*/
 $("#imagen").change(function () {
 
     let imagen = this.files[0];
